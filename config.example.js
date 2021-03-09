557cde2f1a13865c85afd28d42a4e5075c36981b
Andrew Elliott <aelliott@onatlas.com>
2021-03-09 15:45:13 -0800

    add config example

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+	EMAIL_SUBJECT: 'Duga ping alert',
+	EMAIL_TO: 'recipient@domain.com',
+	SENDER_MAIL: 'your+email@domain.com',
+	SENDER_NAME: 'Sender name',
+	SENDER_PASS: 'Email password',
+	URL_TO_CHECK: 'https://'
+};
