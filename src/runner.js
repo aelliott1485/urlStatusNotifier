c6ac2a7f5b223d76dc7e4be1afc8858cca79effc
Andrew Elliott <aelliott@onatlas.com>
2021-07-09 10:49:23 -0700

    add tests, support multiple URLs, handle undefined response

--- src/runner.js
+++ src/runner.js
@@ -2,10 +2,18 @@
 
 const https = require('https');
 const nodemailer = require('nodemailer');
+const configFile = process.argv[2] || 'config';
+const configs = require(__dirname + '/../' + configFile);
 const {checkStatus} = require('./statusChecker');
 
-const checkInterval = setInterval(checkStatus, 60000);
-checkStatus();
-function exceptionHandler(e) {
-    clearInterval(checkInterval);
-}
+configs.forEach(async config => {
+    const boundCall = checkStatus.bind(null, config);
+	const minutes = config.INTERVAL_MINUTES || 1;
+    const checkInterval = setInterval(boundCall, minutes * 60000);
+    await boundCall();
+	//console.log('config after call: ', config)
+    function exceptionHandler(e) {
+        clearInterval(checkInterval);
+    }
+})
+
--- src/statusChecker.js
+++ src/statusChecker.js
@@ -1,63 +1,69 @@
 "use strict";
 
-const https = require('https');
+const axios = require('axios');
 const nodemailer = require('nodemailer');
-const {EMAIL_SUBJECT, EMAIL_TO, SENDER_MAIL, SENDER_NAME, SENDER_PASS, URL_TO_CHECK} = require(__dirname + '/../config');
+/*const {EMAIL_SUBJECT,
+	EMAIL_TO, 
+	EXPECTED_STATUS = 200, 
+	SENDER_MAIL, 
+	SENDER_NAME, 
+	SENDER_PASS, 
+	URL_TO_CHECK
+} = require(__dirname + '/../config');*/
 
 
 module.exports = {
     checkStatus
 };
-let lastCode = 200;
+//let lastCode = EXPECTED_STATUS;
 
-function handleResponse(response) {
-    console.log('statusCode:', response.statusCode);
-    if (response.statusCode > 399 || response.statusCode !== lastCode) {
-        sendEmail(response.statusCode);
+function handleResponse(config, response) {
+	const code = typeof response === 'undefined' ? 0 : response.status;
+    if (code !== (config.lastCode || config.EXPECTED_STATUS)) {
+		console.log('calling sendEmail');
+        sendEmail(config, code);
     }
-    lastCode = response.statusCode;
+    config.lastCode = code;
+	//console.log('----------- set lastCode in config to ', response.statusCode);
 }
-async function sendEmail(code) {
-    const text = `status code from ${URL_TO_CHECK}: ${code}`;
+async function sendEmail(config, code) {
+    const text = `status code from ${config.URL_TO_CHECK}: ${code}`;
     const transporter = nodemailer.createTransport({
         service:'gmail',
         secure: false,
         auth: {
-           user: SENDER_MAIL,
-           pass: SENDER_PASS
+           user: config.SENDER_MAIL,
+           pass: config.SENDER_PASS
         },
         debug: false,
         logger: true
     });
     const info = await transporter.sendMail({
-        from: `"${SENDER_NAME}" <${SENDER_MAIL}>`, // sender address
-        to: EMAIL_TO, // list of receivers
-        subject: EMAIL_SUBJECT, // Subject line
+        from: `"${config.SENDER_NAME}" <${config.SENDER_MAIL}>`, // sender address
+        to: config.EMAIL_TO, // list of receivers
+        subject: config.EMAIL_SUBJECT, // Subject line
         text, // plain text body
         html: `<b>${text}</b>`, // html body
       });
-
-  console.log("Message sent: %s", info.messageId);
+	console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
-function errorHandler(error) {
+function errorHandler(config, error) {
     console.log('error', error);
-    if (lastCode !== error.code) {
-        sendEmail(error.code);
+    if (config.lastCode !== error.code) {
+        sendEmail(config, error.code);
     }
-    lastCode = error.code;
+    config.lastCode = error.code;
     //clearInterval()
 }
 
-function checkStatus() {
-    const event = new Date();
-
-    console.log(`CHECKING STATUS of ${URL_TO_CHECK}`, event.toLocaleString());
-    https.get(URL_TO_CHECK, handleResponse)
-    .on('error', errorHandler);
+async function checkStatus(config) {
+    console.log(`CHECKING STATUS of ${config.URL_TO_CHECK}`, (new Date()).toLocaleString());
+    const response = await axios.get(config.URL_TO_CHECK).catch(errorHandler.bind(null, config));
+	handleResponse(config, response);
 }
 function exceptionHandler(e) {
     clearInterval(checkInterval);
